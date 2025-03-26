444e1849990c2cdba6e1b229c8b440ea5832cf21
Akshay-Anand-Code <85056404+Akshay-Anand-Code@users.noreply.github.com>
2025-03-26 20:14:25 +0530

    links

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -16,9 +16,9 @@ const Navbar = () => {
         <h3 className='text-[0.9rem] leading-5 text-[#dadada]'>Features</h3> */}
       </div>
       <div className="space-x-3 font-inter font-medium">
-        <Link href="https://google.com" target='_blank' className='md:px-6 md:py-2 xs:px-3.5 xs:py-1.5 xss:px-3 xss:py-1.5 hover:scale-105 xss:text-[1rem] hover:bg-[#161616] rounded-md transition-all ease-in-out'>Telegram</Link>
+        <Link href="https://t.me/DeltaAISolana" target='_blank' className='md:px-6 md:py-2 xs:px-3.5 xs:py-1.5 xss:px-3 xss:py-1.5 hover:scale-105 xss:text-[1rem] hover:bg-[#161616] rounded-md transition-all ease-in-out'>Telegram</Link>
         {/* <button className='md:px-4 md:py-[0.37rem] xs:px-3.5 xs:py-1.5 xss:px-3 hover:scale-[1.025] xss:py-1.5 items-center text-black xss:text-[1rem] bg-[#efefef] hover:bg-[#fdfdfd] transition-all ease-in-out rounded-md '>Get Started</button> */}
-        <Link href="https://google.com" target='_blank'>
+        <Link href="https://twitter.com/DeltaAISolana" target='_blank'>
           <RainbowButton>Twitter</RainbowButton>
         </Link>
       </div>
